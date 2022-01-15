cf9e5140fb476ad8f0cefa6df505fa5edaedd478
Jan Koe <97745401+JK-3@users.noreply.github.com>
2022-01-15 14:24:26 +0100

    Delete Press-This-Button directory

--- Press-This-Button/Client_PresentSettingsUI.lua
+++
@@ -1,4 +0,0 @@
-function Client_PresentSettingsUI(rootParent)   
-    horz = UI.CreateHorizontalLayoutGroup(rootParent);
-	UI.CreateLabel(horz).SetText('If the button is not pressed, player income is reduced by (%): '..Mod.Settings.ReducePercent);
-end
