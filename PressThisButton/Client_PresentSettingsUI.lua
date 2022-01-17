d27414ab989a37cc79fe638461285adff3ddef50
Jan Koe <97745401+WarzoneJK3@users.noreply.github.com>
2022-01-17 14:29:52 +0100

    Fixed typo

--- PressThisButton/Client_PresentSettingsUI.lua
+++ PressThisButton/Client_PresentSettingsUI.lua
@@ -1,7 +1,7 @@
 function Client_PresentSettingsUI(rootParent)   
     local vert = UI.CreateVerticalLayoutGroup(rootParent);
 	UI.CreateLabel(vert).SetText('If the button is not pressed, player income is reduced by '..Mod.Settings.ReducePercent..'% the next turn.');
-    UI.CreateLabel(vert).SetText('Popup warning when user failed to press the button: '..YesOrNo(Mod.Settings.PopupWarning)..'.');
+    UI.CreateLabel(vert).SetText('Popup warning when player failed to press the button: '..YesOrNo(Mod.Settings.PopupWarning)..'.');
 end
 
 function YesOrNo(bool)
