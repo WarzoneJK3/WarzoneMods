3d6925444b54619ec43c97760e5bf2c59174df21
Jan Koe <97745401+WarzoneJK3@users.noreply.github.com>
2022-01-17 12:42:26 +0100

    Fixed popup reappearing every 10 seconds

--- PressThisButton/Client_GameRefresh.lua
+++ PressThisButton/Client_GameRefresh.lua
@@ -1,8 +1,8 @@
 function Client_GameRefresh(game)
     firstTurn = not (game.Game.TurnNumber > 1 or (game.Game.TurnNumber == 1 and not game.Settings.AutomaticTerritoryDistribution))
 
-	if game.Us == nil or (not Mod.PlayerGameData.InitialPopupDisplayed ) then
-        UI.Alert("This game includes the Press This Button mod. If you dont press the button your income will be reduced by "..Mod.Settings.ReducePercent..'% next turn.\nPlease note that this also applies to turn 0 (distribution)!')
+	if (not Mod.PlayerGameData.InitialPopupDisplayed ) then
+        UI.Alert("This game includes the Press This Button mod. If you dont press the button your income will be reduced by "..Mod.Settings.ReducePercent..'% next turn.')
 		if (game.Us ~= nil) then
             payload = {};
             payload.msg = "InitialPopupDisplayed";
