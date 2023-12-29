3b8c8c6705c8ef21e9748edd174631ee1bf53a5d
Jan Koe <97745401+WarzoneJK3@users.noreply.github.com>
2023-12-29 23:55:42 +0100

    Remove redundant description of Private Notes
    Mod Description already explains what the mod is about

--- PrivateNotes/Client_PresentConfigureUI.lua
+++
@@ -1,4 +0,0 @@
-function Client_PresentConfigureUI(rootParent)
-    vert = UI.CreateVerticalLayoutGroup(rootParent);
-    UI.CreateLabel(vert).SetText("This mods allows you to take notes.\r\nAccess the mod in the menu by using Game > Private Notes Mod.")
-end
