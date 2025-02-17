c4b2a9e9b9d4a0dec16df43dfafe3ea9c8f459c0
Dan <daniel.church@btinternet.com>
2025-02-17 02:10:59 +0000

    Update Client_SaveConfigureUI.lua
    Press This Button: check for nil before attempting arithmetic

--- PressThisButton/Client_SaveConfigureUI.lua
+++ PressThisButton/Client_SaveConfigureUI.lua
@@ -4,7 +4,7 @@ function Client_SaveConfigureUI(alert)
     Mod.Settings.PopupWarning = warningToggle.GetIsChecked();
     
     -- check if value is within bounds
-    if (Mod.Settings.ReducePercent < 1 or Mod.Settings.ReducePercent > 100 or Mod.Settings.ReducePercent==nil) then 
+    if (Mod.Settings.ReducePercent==nil or Mod.Settings.ReducePercent < 1 or Mod.Settings.ReducePercent > 100) then 
         alert('"Reduce player income by" must be set between 1 and 100'); 
     end
 end
